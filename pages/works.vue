238df6a0e018d9f9ee5eeb1411b877bc4a3d94ff
masayukinii1011 <masayukinii1011@gmail.com>
2020-06-20 16:23:48 +0900

    worksページデータ取得

--- pages/works.vue
+++ pages/works.vue
@@ -1,23 +1,55 @@
 <template>
   <div>
-    <PostDetail
-      :title="$nuxt.$route.name.toUpperCase()"
-      :body="$md.render(about.fields.body)"
-      :githubLink="about.fields.githubLink"
-    />
+    <section class="section">
+      <div class="container">
+        <section class="section">
+          <div class="container has-text-centered">
+            <h2 class="has-text-weight-bold title">{{$nuxt.$route.name.toUpperCase()}}</h2>
+          </div>
+        </section>
+        <div v-if="works.length" class="columns is-multiline is-variable is-4">
+          <div v-for="work in works" :key="work.fields.id" class="column is-half">
+            <div class="card">
+              <div>{{work.fields.publishDate}}</div>
+              <div>{{work.fields.link}}</div>
+              <div>{{work.fields.body}}</div>
+              <div class="card-image">
+                <figure class="image is-2by1">
+                  <img
+                    v-if="work.fields.image"
+                    :src="work.fields.image.fields.file.url"
+                    :alt="work.fields.image.fields.title"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <p class="title is-size-4 has-text-centered">{{work.fields.title}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p class="is-size-4" v-else>There is no post.</p>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import PostDetail from "~/components/PostDetail.vue";
 
 export default {
-  components: {
-    PostDetail
-  },
   computed: {
-    ...mapState(["about"])
+    ...mapState(["works"])
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.card-content {
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid #eee;
+}
+</style>
